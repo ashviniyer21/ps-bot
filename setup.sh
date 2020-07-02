1ff7302aee726bbb120ff27dec41253404365939
Ashvin Iyer <37090551+ashviniyer21@users.noreply.github.com>
2020-07-02 10:41:01 -0700

    Making setup.sh more clear in installation

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #chmod +x setup.sh
 which pip
-echo "Installing libraries"
+echo "Installing libraries 0/3"
 pip install selenium
+echo "Installing libraries 1/3"
 pip install chromedriver-binary
+echo "Installing libraries 2/3"
 pip install requests
-echo "Finished Installing"
+echo "Installing libraries 3/3"
