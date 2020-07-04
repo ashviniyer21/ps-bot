5f8e37ae4979a3316cec933fd767a6b6a5755e33
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-07-04 09:30:36 -0700

    Move Rehaul pt. 1
    Basic info for moves retrieved, secondary effects and differentiating between self stats and opponent stats still necesary

--- move.py
+++ move.py
@@ -29,6 +29,99 @@ class Move:
         print()
         print()
         print()
+def get_all_moves2():
+    url='http://play.pokemonshowdown.com/data/moves.js'
+    page = requests.get(url)
+    text = page.text[24:]
+    text = text[:len(text)-1]
+    counter = 0
+    while(text.find("accuracy:") != -1):
+        index1 = text.find("accuracy:")
+        text = text[index1:]
+        index1 = text.find("accuracy:") + 9
+        index2 = text.find(",")
+        accuracy = -1
+        try:
+            accuracy = int(text[index1: index2])
+        except:
+            pass
+        text = text[index2 + 1:]
+        index1 = text.find("basePower:") + 10
+        index2 = text.find(",")
+        power = 0
+        try:
+            power = int(text[index1: index2])
+        except:
+            pass
+        text = text[index2 + 1:]
+        index1 = text.find("category:\"") + 10
+        index2 = text.find("\",")
+        category = text[index1: index2]
+        index1 = text.find("shortDesc:")
+        text = text[index1:]
+        index1 = text.find("shortDesc:\"") + 11
+        index2 = text.find("\",")
+        description = text[index1: index2]
+        index1 = text.find("name:\"")
+        text = text[index1:]
+        index1 = text.find("name:\"") + 6
+        index2 = text.find("\",")
+        name = text[index1: index2]
+        index1 = text.find("priority:")
+        text = text[index1:]
+        index1 = text.find("priority:") + 9
+        index2 = text.find(",")
+        priority = int(text[index1: index2])
+        boosts = [0, 0, 0, 0, 0]
+        if(text.find(",boosts:") < text.find(",type:\"") and text.find(",boosts:") < text.find("secondary:") and text.find(",boosts:") != -1):
+            index1 = text.find(",boosts:") + 1
+            text = text[index1:]
+            index1 = text.find("boosts:{") + 8
+            index2 = text.find("}")
+            boosts_text = text[index1: index2].split(",")
+            for boost_text in boosts_text:
+                boost_values = boost_text.split(":")
+                if(boost_values[0] == "atk"):
+                    boosts[0] = int(boost_values[1])
+                elif(boost_values[0] == "def"):
+                    boosts[1] = int(boost_values[1])
+                elif(boost_values[0] == "spa"):
+                    boosts[2] = int(boost_values[1])
+                elif(boost_values[0] == "spd"):
+                    boosts[3] = int(boost_values[1])
+                elif(boost_values[0] == "spe"):
+                    boosts[4] = int(boost_values[1])
+        num_hit = 1
+        if(text.find("multihit:") < text.find(",type:\"") and text.find("multihit:") != -1):
+            index1 = text.find("multihit:")
+            text = text[index1:]
+            index1 = text.find("multihit:") + 9
+            index2 = text.find(",")
+            num_hit = int(text[index1:index2])
+        if(text.find("status:") < text.find(",type:\"") and text.find("status:") != -1):
+            index1 = text.find("status:")
+            text = text[index1:]
+            index1 = text.find("status:") + 7
+            index2 = text.find(",")
+            status = text[index1:index2]
+        index2 = text.find("},")
+        index1 = text.find(",type:\"") + 1
+        text = text[index1:]
+        index1 = text.find("type:\"") + 6
+        index2 = text.find("\",")
+        move_type = text[index1:index2].lower()
+        if(counter < 20):
+            print(accuracy)
+            print(power)
+            print(category)
+            print(description)
+            print(name)
+            print(priority)
+            print(boosts)
+            print(secondary_text)
+            print(move_type)
+            print()
+        counter += 1
 
 def get_all_moves():
     url='http://pokemondb.net/move/all'
@@ -201,3 +294,4 @@ def get_move(name):
         if(all_moves[i].name.lower().replace(" ", "").replace("-", "") == name.lower() or all_moves[i].name.lower().replace(" ", "").replace("-", "") == name[:len(name)-1].lower()):
             return all_moves[i]
     return all_moves[0]
+get_all_moves2()
