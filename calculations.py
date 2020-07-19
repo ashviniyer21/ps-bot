8655d7c768c88056a7b3a91a4eaa573a9112232e
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2020-07-19 15:17:43 -0700

    Changing Calculations to a priority based system
    Will make calculations slightly worse for now, but make it much easier to implement new features (status, weather, etc.) in the future

--- calculations.py
+++ calculations.py
@@ -1,16 +1,15 @@
-from move import Move
-from pokemon import Pokemon
-from pokemon import EnemyPokemon
 from move import get_move
 from pokemon import get_pokemon
 import random
+
+
 def calc_type_matchup(move_type, pokemon_types):
     chart = dict()
 
     normal_chart = dict()
     normal_chart["strength"] = list()
-    normal_chart["weakness"] =  ["rock", "steel"]
-    normal_chart["immunity"] =  ["ghost"]
+    normal_chart["weakness"] = ["rock", "steel"]
+    normal_chart["immunity"] = ["ghost"]
     chart["normal"] = normal_chart
 
     fire_chart = dict()
@@ -22,7 +21,7 @@ def calc_type_matchup(move_type, pokemon_types):
     water_chart = dict()
     water_chart["strength"] = ["fire", "ground", "rock"]
     water_chart["weakness"] = ["water", "grass", "dragon"]
-    water_chart["immunity"]  = list()
+    water_chart["immunity"] = list()
     chart["water"] = water_chart
 
     electric_chart = dict()
@@ -114,17 +113,18 @@ def calc_type_matchup(move_type, pokemon_types):
     fairy_chart["weakness"] = ["fire", "poison", "steel"]
     fairy_chart["immunity"] = []
     chart["fairy"] = fairy_chart
-    
+
     mult = 1.0
     the_chart = chart[move_type]
     for pkmn_type in pokemon_types:
-        if(pkmn_type in the_chart["strength"]):
+        if (pkmn_type in the_chart["strength"]):
             mult *= 2.0
-        elif(pkmn_type in the_chart["weakness"]):
+        elif (pkmn_type in the_chart["weakness"]):
             mult *= 0.5
-        elif(pkmn_type in the_chart["immunity"]):
+        elif (pkmn_type in the_chart["immunity"]):
             return 0
     return mult
+
 
 def basic_calculate(my_pokemon, enemy_pokemon, enemy_level):
     best = 0
@@ -138,61 +138,61 @@ def basic_calculate(my_pokemon, enemy_pokemon, enemy_level):
         def_max = 0.0
         min_damage = 0.0
         max_damage = 0.0
-        if(temp_move.category == "Physical"):
+        if (temp_move.category == "Physical"):
             atk = my_pokemon.attack
-            def_min = int((2 * enemy_pokemon.stats[2] + 31 + 85/4.0) * enemy_level)
+            def_min = int((2 * enemy_pokemon.stats[2] + 31 + 85 / 4.0) * enemy_level)
             def_min /= 100.0
             def_min += 5
-            def_max = int((2 * enemy_pokemon.stats[2] + 31 + 85/4.0) * enemy_level)
+            def_max = int((2 * enemy_pokemon.stats[2] + 31 + 85 / 4.0) * enemy_level)
             def_max /= 100.0
             def_max += 5
-        elif(temp_move.category == "Special"):
+        elif (temp_move.category == "Special"):
             atk = my_pokemon.special_attack
-            def_min = int((2 * enemy_pokemon.stats[4] + 31 + 85/4.0) * enemy_level)
+            def_min = int((2 * enemy_pokemon.stats[4] + 31 + 85 / 4.0) * enemy_level)
             def_min /= 100.0
             def_min += 5
-            def_max = int((2 * enemy_pokemon.stats[4] + 31 + 85/4.0) * enemy_level)
+            def_max = int((2 * enemy_pokemon.stats[4] + 31 + 85 / 4.0) * enemy_level)
             def_max /= 100.0
             def_max += 5
-        if(temp_move.category != "Status"):
+        if (temp_move.category != "Status"):
             used_level = 0
             try:
                 used_level = int(my_pokemon.level)
             except:
                 used_level = 100
-            min_damage = 2 + ((temp_move.power * atk/def_max * (2 + (2 * used_level/5.0)))/50.0)
-            max_damage = 2 + ((temp_move.power * atk/def_min * (2 + (2 * used_level/5.0)))/50.0)
+            min_damage = 2 + ((temp_move.power * atk / def_max * (2 + (2 * used_level / 5.0))) / 50.0)
+            max_damage = 2 + ((temp_move.power * atk / def_min * (2 + (2 * used_level / 5.0))) / 50.0)
             stab = 1.0
             quick_testing_types = get_pokemon(my_pokemon.name).types
-            if(temp_move.move_type in quick_testing_types):
+            if (temp_move.move_type in quick_testing_types):
                 stab = 1.5
             min_damage *= calc_type_matchup(temp_move.move_type, enemy_pokemon.types) * stab * 0.85 * temp_move.num_hit
             max_damage *= calc_type_matchup(temp_move.move_type, enemy_pokemon.types) * stab * temp_move.num_hit
-        hp_min = (((2 * enemy_pokemon.stats[0] + 31 + (85/4.0)) * enemy_level)/100.0) + enemy_level + 10
-        hp_max = (((2 * enemy_pokemon.stats[0] + 31 + (85/4.0)) * enemy_level)/100.0) + enemy_level + 10
+        hp_min = (((2 * enemy_pokemon.stats[0] + 31 + (85 / 4.0)) * enemy_level) / 100.0) + enemy_level + 10
+        hp_max = (((2 * enemy_pokemon.stats[0] + 31 + (85 / 4.0)) * enemy_level) / 100.0) + enemy_level + 10
         min_damage /= hp_max
         min_damage *= 1000
-        min_damage = round(min_damage)/10.0
+        min_damage = round(min_damage) / 10.0
         max_damage /= hp_min
         max_damage *= 1000
-        max_damage = round(max_damage)/10.0
+        max_damage = round(max_damage) / 10.0
         print("Damage: ", temp_move.name)
         print("Min: ", min_damage, " Max: ", max_damage)
-        the_damage.append((min_damage + max_damage)/2.0)
+        the_damage.append((min_damage + max_damage) / 2.0)
     damage_indexes = list()
     new_the_damage = sorted(the_damage)
     for i in range(len(new_the_damage)):
         for j in range(len(the_damage)):
-            if(new_the_damage[i] == the_damage[j] and not j in damage_indexes):
+            if (new_the_damage[i] == the_damage[j] and not j in damage_indexes):
                 damage_indexes.append(j)
     damage_indexes.reverse()
     return damage_indexes
+
 
 def advance_calculate(my_pokemon, enemy_pokemon, enemy_level, self_boosts, opp_boosts, enemy_hp):
     damage_values = list()
     for move in my_pokemon.moves:
         actual_move = get_move(move)
-        damage = {0, 0}
         damage1 = 0
         damage2 = 0
         level = 100
@@ -200,45 +200,45 @@ def advance_calculate(my_pokemon, enemy_pokemon, enemy_level, self_boosts, opp_b
             level = int(enemy_level)
         except:
             pass
-        if(actual_move.category != "Status"):
+        if (actual_move.category != "Status"):
             attack = 0
             defense = 0
-            if(actual_move.category == "Physical"):
+            if (actual_move.category == "Physical"):
                 attack = my_pokemon.attack
-                if(my_pokemon.item.lower() == "choice band"):
+                if (my_pokemon.item.lower() == "choice band"):
                     attack *= 1.5
-                if(self_boosts[0] > 0):
+                if (self_boosts[0] > 0):
                     attack *= 1 + 0.5 * self_boosts[0]
-                elif(self_boosts[0] < 0):
-                    attack *= 2.0/(2.0-self_boosts[0])
-                defense = round((2 * enemy_pokemon.stats[2] + 31 + 85/4.0) * level)
-                if(opp_boosts[1] > 0):
+                elif (self_boosts[0] < 0):
+                    attack *= 2.0 / (2.0 - self_boosts[0])
+                defense = round((2 * enemy_pokemon.stats[2] + 31 + 85 / 4.0) * level)
+                if (opp_boosts[1] > 0):
                     defense *= 1 + 0.5 * opp_boosts[1]
-                elif(opp_boosts[1] < 0):
-                    defense *= 2.0/(2.0-opp_boosts[1])
-            elif(actual_move.category == "Special"):
+                elif (opp_boosts[1] < 0):
+                    defense *= 2.0 / (2.0 - opp_boosts[1])
+            elif (actual_move.category == "Special"):
                 attack = my_pokemon.special_attack
-                if(self_boosts[2] > 0):
+                if (self_boosts[2] > 0):
                     attack *= 1 + 0.5 * self_boosts[2]
-                elif(self_boosts[2] < 0):
-                    attack *= 2.0/(2.0-self_boosts[2])
-                if(my_pokemon.item.lower() == "choice specs"):
+                elif (self_boosts[2] < 0):
+                    attack *= 2.0 / (2.0 - self_boosts[2])
+                if (my_pokemon.item.lower() == "choice specs"):
                     attack *= 1.5
-                defense = round((2 * enemy_pokemon.stats[4] + 31 + 85/4.0) * level)
-                if(opp_boosts[3] > 0):
+                defense = round((2 * enemy_pokemon.stats[4] + 31 + 85 / 4.0) * level)
+                if (opp_boosts[3] > 0):
                     defense *= 1 + 0.5 * opp_boosts[3]
-                elif(opp_boosts[3] < 0):
-                    defense *= 2.0/(2.0-opp_boosts[3])
+                elif (opp_boosts[3] < 0):
+                    defense *= 2.0 / (2.0 - opp_boosts[3])
             defense /= 100.0
             defense += 5
             stab = 1
-            if(actual_move.move_type in get_pokemon(my_pokemon.name).types):
+            if (actual_move.move_type in get_pokemon(my_pokemon.name).types):
                 stab = 1.5
-            damage_reg = 2 + ((actual_move.power * attack/defense * (2 + (2 * level/5.0)))/50.0)
+            damage_reg = 2 + ((actual_move.power * attack / defense * (2 + (2 * level / 5.0))) / 50.0)
             damage_reg *= calc_type_matchup(actual_move.move_type, enemy_pokemon.types) * stab * actual_move.num_hit
-            if(my_pokemon.item.lower() == "life orb"):
+            if (my_pokemon.item.lower() == "life orb"):
                 damage_reg *= 1.3
-            hp = round((((2 * enemy_pokemon.stats[0] + 31 + (85/4.0)) * level)/100.0) + level + 10)
+            hp = round((((2 * enemy_pokemon.stats[0] + 31 + (85 / 4.0)) * level) / 100.0) + level + 10)
             damage1 = damage_reg * 0.85
             damage1 /= hp
             damage1 *= 1000.0
@@ -249,22 +249,35 @@ def advance_calculate(my_pokemon, enemy_pokemon, enemy_level, self_boosts, opp_b
             damage2 *= 1000.0
             damage2 = round(damage2)
             damage2 /= 10.0
-            damage = {damage1, damage2}
         print(move)
         print("Damage1: ", damage1, " Damage2: ", damage2)
         damage_values.append(damage1)
-    #print(damage_values)
-    temp_damage = list()
-    for value in damage_values:
-        temp_damage.append(value)
-    temp_damage2 = sorted(temp_damage)
-    damage_indexes = list()
-    for i in range(len(temp_damage2)):
-        for j in range(len(temp_damage)):
-            if(temp_damage2[i] == temp_damage[j] and not j in damage_indexes):
-                damage_indexes.append(j)
-    damage_indexes.reverse()
-    return damage_indexes
+    # print(damage_values)
+    priority_indexes = list()
+    move_indexes = list()
+    for i in range(4):
+        priority_indexes.append(0)
+        if(damage_values[i] > enemy_hp):
+            priority_indexes[i] = 10
+        elif(damage_values[i] > enemy_hp / 2):
+            priority_indexes[i] = 8
+        elif(damage_values[i] > enemy_hp / 3):
+            priority_indexes[i] = 6
+        elif (damage_values[i] > enemy_hp / 4):
+            priority_indexes[i] = 4
+        elif (damage_values[i] > enemy_hp / 5):
+            priority_indexes[i] = 2
+        actual_move = get_move(my_pokemon.moves[i])
+        if(actual_move.priority > 0):
+            priority_indexes[i] += 1
+    for i in range(4):
+        temp_index = max(priority_indexes)
+        for j in range(4):
+            if(priority_indexes[j] == temp_index and not j in move_indexes):
+                move_indexes.append(j)
+                priority_indexes[j] = 0
+                break
+    return move_indexes
 
 def calculate_random():
     damage_indexes = list([0, 1, 2, 3])
